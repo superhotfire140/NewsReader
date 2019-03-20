f946f4d461eeee6843b726c8400f7d4622b7c919
superhotfire140 <35212758+superhotfire140@users.noreply.github.com>
2019-03-20 12:44:13 -0700

    Update newsreader.py

--- newsreader.py
+++ newsreader.py
@@ -1,26 +1,63 @@
 import requests
 from bs4 import BeautifulSoup as soup
 
-source = requests.get('https://www.npr.org/sections/news/').text
+class Headlines:
+    """
+    Object to contain the title, url, and text."""
+    def __init__(self, otitle, olink):
+        self.title = otitle
+        self.link = olink
+        self.article = []
+    def get_Article(self):
+        key = soup(requests.get(self.link).text, 'lxml')
+        foo = key.find('div', {'class': 'storytext storylocation linkLocation'})
+        xparagraph = foo.findAll('p')
+        bar = []
+        for x in xparagraph:
+            bar.append(x.get_text())
+        self.article = bar
 
-npr_soup = soup(source, 'lxml')
+sources = requests.get('https://www.npr.org/sections/news/').text
+
+npr_soup = soup(sources, 'lxml')
 
 data = npr_soup.find('div', {'class': 'list-overflow'})
 titles = data.findAll('h2', {'class': 'title'})
 
-def printInfo():
-    for t in titles:
-        print(t.get_text())
+xtitles = []
+for t in titles:
+    xtitles.append(t.get_text())
 
+xlinks = []
+for t in titles:
+    foo = t.find('a')
+    xlinks.append(foo.get('href'))
+
+xheadlines = []
+for t, l in zip(xtitles, xlinks):
+    article = Headlines(t,l)
+    xheadlines.append(article)
+
+for x in xheadlines:
+    x.get_Article()
+
+def printInfo():
+    for h in xheadlines:
+        print(h.title)
+        print(h.link)
+
+#########################
 
 print('----------')
 printInfo()
 print('----------')
 
-
 file = open('articles.txt','w')
-for t in titles:
-    file.write(str(t.get_text()))
+
+for x in xheadlines:
+    file.write(str(x.title))
+    file.write("\n")
+    file.write(str(x.link))
     file.write('\n')
-    file.write(str('----------'))
-    file.write('\n')
+    for bar in x.article:
+        file.write(str(bar))
